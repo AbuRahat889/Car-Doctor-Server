5968d80a2c58534b05344004653fd490ead93e8e
AbuRahat889 <aburahatshaum889@gmail.com>
2024-05-10 01:00:23 +0600

    final added

--- index.js
+++ index.js
@@ -15,12 +15,32 @@ const port = process.env.PORT || 5000;
 //middlewar
 app.use(
   cors({
-    origin: ["http://localhost:5173", 'http://localhost:5174'],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
+
+//custom middelwair
+const logger = (req, res, next) => {
+  console.log("loger info : ", req.method, req.url);
+  next();
+};
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("custom middleware : ", token);
+  if (!token) {
+    return req.status(401).send({ message: "unauthoraized access!" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decode) => {
+    if (error) {
+      return req.status(401).send({ message: "unauthorize access!" });
+    }
+    req.user = decode;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8jmnni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -43,11 +63,11 @@ async function run() {
       .db("carsDoctor")
       .collection("checkOutInfo");
 
-    //auth info jwt
+    //auth info jwt create token using cookies
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET, {
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
 
@@ -55,8 +75,15 @@ async function run() {
         .cookie("token", token, {
           httpOnly: true,
           secure: false,
+          sameSite: "strict",
         })
         .send({ success: true });
+    });
+    //clear cookies if user logout
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("log out user  ");
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
     });
 
     //services info
@@ -86,9 +113,14 @@ async function run() {
     });
 
     //finding user for email cart page
-    app.get("/checkout", async (req, res) => {
+    app.get("/checkout", logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
-      console.log("token is : ", req.cookies.token);
+      if (req.user.email !== req.query.email) {
+        return req.status(403).send({ message: "forbidden access!!" });
+      }
+
+      console.log("token owner info  : ", req.user);
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
